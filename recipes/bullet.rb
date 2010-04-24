8cba80243e0828ca9c1d802c22992914f8d8d45c
Sven Riedel <sr@gimp.org>
2010-04-24 16:11:57 +0200

    Change calls to environment to inject_into_file due to environment bugginess

--- recipes/bullet.rb
+++ recipes/bullet.rb
@@ -2,25 +2,27 @@
 # Bullet
 #
 say "Install bullet"
-gem "bullet", "=~ 1.7.6", :group => "development"
-append_file File.join( %W(config environments development.rb) ), <<-EOF
-config.after_initialize do
-  Bullet.enable = true 
-  Bullet.alert = true
-  Bullet.bullet_logger = true  
-  Bullet.console = true
-  Bullet.growl = false
-  Bullet.rails_logger = true
-  Bullet.disable_browser_cache = true
-end
+gem "bullet", :group => "development"
+inject_into_file File.join( %W( config environments development.rb ) ),
+                 :before => %r{^end$} do
+%{
+  config.after_initialize do
+    Bullet.enable = true 
+    Bullet.alert = true
+    Bullet.bullet_logger = true  
+    Bullet.console = true
+    Bullet.growl = false
+    Bullet.rails_logger = true
+    Bullet.disable_browser_cache = true
+  end
 
-begin
-  require 'ruby-growl'
-  Bullet.growl = true
-rescue MissingSourceFile
+  begin
+    require 'ruby-growl'
+    Bullet.growl = true
+  rescue MissingSourceFile
+  end
+}
 end
- 
-EOF
 
 git :add => "."
 git :commit => "-m 'Installed Bullet'"
--- recipes/devise.rb
+++ recipes/devise.rb
@@ -6,7 +6,12 @@ gem "devise", "1.1.rc1"
 run "bundle install"
 generate "devise_install"
 gsub_file File.join( %W(config routes.rb) ), %r{# root :to }, "root :to "
-environment "  config.action_mailer.default_url_options = { :host => 'localhost:3000' }", :env => :development
+inject_into_file File.join( %W(config environments development.rb ) ),
+                 :before => %r{^end$} do
+  %{  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
+}
+end
 inject_into_file File.join( %W( app views layouts application.html.erb ) ),
                  %{  <p class="notice"><%= notice %></p>
                      <p class="alert"><%= alert %></p>
